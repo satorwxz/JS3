2d883ef20de8ca8c49eac83f5359ed8374920f19
satorwxz <bekakasymbekov23@gmail.com>
2024-07-30 15:12:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,249 @@
+//Заданние 1
+// user admin true welcome, 'user'
+// false not correct
+
+
+//const a='user'
+//function handleCheck(){
+   //if (a === 'admin'){
+        //return 'welcome, admin'
+   //}else
+        //return ' false not correct user'
+//}
+
+//console.log(handleCheck())
+
+//Задание 2
+// today is 'day' of february
+// on february only 28 day, your day is 'day' of another month
+
+//const a=4
+
+ //function handleCheck(){
+    //if (a <= 28 && a >= 0){
+        //return `today is ${a} day of february`
+    //}else{
+        //return `on february only 28 day, your ${a} day is day of another month`
+   //}
+
+ //}
+//console.log(handleCheck())
+
+
+
+//Задание 3
+// 'num' is equals of 50
+// 'num' is not correct
+
+//const a=50
+//function handleCheck(){
+    //if (a === 50){
+        //return `${a} is equals of 50`
+    //}else
+        //return `${a} is not correct`
+//}
+
+//console.log(handleCheck())
+
+
+//Задание 4
+// 'user' 'age' old, welcome
+// 'user' 'age' old, rejected
+
+//const a='Jack'
+//const b=27
+
+//function handleCheck(){
+    //if (b >= 18){
+        //return `${a},${b} old,welcome`
+    //}else {
+        //return `${a},${b} old,rejected`
+    //}
+//}
+
+//console.log(handleCheck())
+
+
+
+//Задание 5
+// 'user' name is too short (min 3)
+// 'user' name is accepted
+
+// const a='GIY'
+//
+// function handleCheck(){
+//     if (a.length <= 3){
+//         return `'${a}' name is too short`
+//     }else{
+//         return `'${a}' name is accepted`
+//     }
+// }
+//
+// console.log(handleCheck())
+
+
+
+//Задание 6
+// 'user' name is too long (max 5)
+// 'user' name is accepted
+
+
+// const a='Bektur'
+//
+// function handleCheck(){
+// if (a.length > 5){
+//  return `'${a}' name is too long (max 5)`
+//     }else{
+//          return `'${a}' name is accepted`
+//         }
+//     }
+//
+// console.log(handleCheck())
+
+
+
+//Задание 7
+// 'age' must be a number ('20')
+
+// const a=20
+// const b= String(a)
+//
+// console.log(typeof b + ' ' + b)
+
+//Задание 8
+// 'age' must be a string (20)
+
+
+//const a='20'
+//const b= Number(a)
+
+//console.log(typeof b + ' ' + b)
+
+
+
+//Задание 9
+// 'age' must be older then 23
+
+// const a=24
+//
+// function handleCheck(){
+//     if (a <= 23){
+//         return 'age must be older then 23'
+//     }else{
+//         return 'true'
+//
+//     }
+// }
+//
+// console.log(handleCheck())
+
+
+
+//Задание 10
+// today is monday/tuesday/wednesday/thursday/friday weekend
+// max days of weeks 7
+
+
+//const a='Monjnkjkjny'
+
+//function handleCheck(){
+    //if ('monday' &&  'tuesday' && 'wednesday' && 'thursday' && 'friday' && 'Saturday' && 'Sunday'){
+        //return `today is ${a} `
+    //}else {
+        //return ' max days of weeks 7'
+   // }
+//}
+
+//console.log(handleCheck())
+
+
+//Задание 11
+// hello world
+// 1. вам надо будет проверить и убрать пробелы в начале и в конце
+// 2. поменять местами world hello
+// 3. не первое o поменять на а
+
+//Задание 1
+
+// let a=' hello, world  '
+//
+// function handleCheck(){
+//     if (a.trim() !== ''){
+//         return a.trim()
+//
+//     }else {
+//         return 'false'
+//
+//     }
+// }
+//
+// console.log(handleCheck(a))
+
+
+//Задание 2
+//let str='hello world'
+//let arr=str.split(' ').reverse().join(' ')
+
+//console.log(arr)
+
+
+
+//Задание 3
+
+// let str = 'hello world'
+// let str2=str.replaceAll('o', 'a')
+//
+// console.log(str2)
+
+
+//Задание 4
+// 1. проверить длинну на четность
+
+//let str = 'hello world'
+
+//function handleCheck(){
+   // if(str.length % 2 === 0){
+       // return 'true'
+  //  }else
+      //  return 'false'
+//}
+
+//console.log(handleCheck())
+
+
+
+
+//Задание 5
+// 1. lorem ipsum dolor sit amet => loremipsumdolorsitamet
+// price и discount
+
+// let str = 'lorem ipsum dolor sit amet'
+// let str2 = str.replaceAll(' ','')
+//
+// console.log(str2)
+
+//Задание 6
+//// str в задаче вам надо проверить что это строка.
+// const str=54
+// function handleCheck(){
+//     if(typeof str === 'string'){
+//         return 'true'
+//     }else{
+//         return 'false'
+//     }
+// }
+//
+// console.log(handleCheck())
+// // вы проверяете не пустая ли она. длина мин 4 макс 10. четность длины.
+
+// const str='JNKlkml'
+// function handleCheck(){
+//     if(typeof str === 'string' && str.length % 2 === 0 && str.length >=4 && str.length <= 10){
+//         return 'true'
+//     }else{
+//         return 'false'
+//     }
+// }
+//
+// console.log(handleCheck())
+
